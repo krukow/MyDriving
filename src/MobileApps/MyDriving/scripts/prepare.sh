57fad9b38b6b1e4b2f21d4fd5d1236ae74573024
Karl Krukow <kakrukow@microsoft.com>
2017-09-20 19:52:57 +0800

    Support Android tests

--- src/MobileApps/MyDriving/scripts/prepare.sh
+++ src/MobileApps/MyDriving/scripts/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p $1
+UITEST_VERSION="2.1.1"
 echo "Preparing artifacts dir $1"
-mono ../packages/Xamarin.UITest.2.0.7/tools/test-cloud.exe prepare MyDriving.Android/bin/XTC/com.microsoft.mydriving-Signed.apk keystore MyDriving.Android/Build2016.keystore "Build*1234" Build2016 "Build*1234" --assembly-dir MyDriving.UITests/bin/XTC --artifacts-dir $1
+mono ../packages/Xamarin.UITest.$UITEST_VERSION/tools/test-cloud.exe prepare MyDriving.Android/bin/XTC/com.microsoft.mydriving-Signed.apk keystore MyDriving.Android/Build2016.keystore "Build*1234" Build2016 "Build*1234" --assembly-dir MyDriving.UITests/bin/XTC --artifacts-dir $1 --test-parameter "screencapture=true"
--- src/MobileApps/MyDriving/scripts/run-prepared.sh
+++ src/MobileApps/MyDriving/scripts/run-prepared.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running tests on devices $2 from prepared dir $1"
-mobile-center test run manifest --manifest-path $1/manifest.json --app-path MyDriving.Android/bin/XTC/com.microsoft.mydriving-Signed.apk --devices $2 --test-series "mc-demo-april-2017" --app "Continuous-Delivery-Samples/MyDriving-02" --disable-telemetry
+mobile-center test run manifest --manifest-path $1/manifest.json --app-path MyDriving.Android/bin/XTC/com.microsoft.mydriving-Signed.apk --devices "$2" --test-series "master" --app "MonkeyFest-Singapore-2017/MyDriving-for-Android" --disable-telemetry
