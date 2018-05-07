933e2f1899397026f4b3f71c9b15de8d16e5ff20
Karl Krukow <kakrukow@microsoft.com>
2018-05-07 11:51:43 +0200

    Use older devices device set

--- src/MobileApps/appcenter-post-build.sh
+++ src/MobileApps/appcenter-post-build.sh
@@ -4,7 +4,7 @@ then
     msbuild /p:Configuration=Debug $APPCENTER_SOURCE_DIRECTORY/src/MobileApps/MyDriving/MyDriving.UITests/MyDriving.UITests.csproj
     appcenter test run uitest \
                 --app "GotoCPH-2017/MyDriving-for-iOS" \
-                --devices "MonkeyFest-Singapore-2017/ios-smoketest-devices" \
+                --devices "GotoCPH-2017/older-devices" \
                 --app-path $APPCENTER_OUTPUT_DIRECTORY/MyDrivingiOS.ipa \
                 --test-series "master" \
                 --locale "en_US" \
